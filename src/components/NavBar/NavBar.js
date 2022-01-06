e6490b79307e12c02eb4fe5d8bc1e35b3b153c19
Victor Rosas <victorrosas@MacBook-Pro-de-Victor.local>
2022-01-06 00:35:28 -0500

    started navbar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function NavBar() {
+  return (
+      <Container>
+          <h1>NavBar</h1>
+      </Container>
+  )
+}
+
+export default NavBar;
---
+++ src/components/NavBar/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.nav`
+    background-color: #F2F2F2;
+    height: 134px;
+    min-height: 50px;
+    position: relative;
+    display: block;
+    z-index: 2;
+`;
